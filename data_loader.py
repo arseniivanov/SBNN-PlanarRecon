511daa6fefa9fec9016acb7e5176c4eff6e1dd08
Arseni <arseni_mars97@hotmail.com>
2023-10-10 22:20:29 +0200

    Replaced MNIST with KTH actions as they are labelled for classification

--- data_loader.py
+++ data_loader.py
@@ -1,44 +1,43 @@
-# Import necessary libraries for creating the dataloader
-import numpy as np
+import cv2
 import torch
-from torch.utils.data import Dataset
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
 
-# Define the Moving MNIST dataset class
-class MovingMNIST(Dataset):
-    def __init__(self, npy_file_path):
-        # Load the .npy file
-        self.data = np.load(npy_file_path)
-        
-        # Move the data to channel-last format to channel-first format
-        # Original shape: (num_samples, sequence_length, height, width)
-        # New shape: (num_samples, sequence_length, 1, height, width)
-        self.data = np.expand_dims(self.data, axis=2)
-        
+class KTHDataset(Dataset):
+    def __init__(self, video_list, frame_number=400,transform=None):
+        self.video_list = video_list
+        self.transform = transform
+        self.FIXED_NUM_FRAMES = frame_number
+
     def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        # Fetch the sequence at the given index
-        sequence = self.data[idx]
-        
-        # Convert the sequence to PyTorch tensor
-        sequence_tensor = torch.FloatTensor(sequence)
-        
-        # For object classification, you may want to return the first 10 frames as input and the last 10 frames as target
-        input_tensor = sequence_tensor[:10]
-        target_tensor = sequence_tensor[10:]
-        
-        return input_tensor, target_tensor
+        return len(self.video_list)
 
-def rgb_to_yuv(img):
-    if isinstance(img, torch.Tensor):
-        if len(img.shape) == 4:
-            img = img.permute(0, 2, 3, 1)  # Change (B,C,H,W) to (B,H,W,C) for pytorch tensor
-        else:
-            img = img.permute(1, 2, 0)  # Change (C,H,W) to (H,W,C) for pytorch tensor
-    img = img.to(torch.float32)
-    in_img = img.contiguous().view(-1, 3)
-    out_img = torch.mm(in_img, torch.tensor([[0.299, -0.14713, 0.615],
-                                             [0.587, -0.28886, -0.51499],
-                                             [0.114, 0.436, -0.10001]]).t()).view(*img.shape)
-    return out_img
+    def __getitem__(self, idx):
+        path, label = self.video_list[idx]
+        cap = cv2.VideoCapture(path)
+        frames = []
+
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret:
+                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                frames.append(gray_frame)
+            else:
+                break
+
+        cap.release()
+
+        # Normalize the number of frames to FIXED_NUM_FRAMES
+        if len(frames) > self.FIXED_NUM_FRAMES:
+            frames = frames[:self.FIXED_NUM_FRAMES]  # Truncate
+        elif len(frames) < self.FIXED_NUM_FRAMES:
+            # Zero-pad
+            while len(frames) < self.FIXED_NUM_FRAMES:
+                frames.append(np.zeros((120, 160), dtype=np.uint8))
+
+        frames = np.array(frames)
+        if self.transform:
+            frames = self.transform(frames)
+
+        return torch.tensor(frames, dtype=torch.float32), torch.tensor(label, dtype=torch.long)
+
--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,38 @@
-from data_loader import MovingMNIST
+import os
+from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
+from data_loader import KTHDataset
 
-# Create an instance of the dataset
-moving_mnist_dataset = MovingMNIST("data/moving_mnist.npy")
 
-# Create a dataloader
-dataloader = DataLoader(moving_mnist_dataset, batch_size=32, shuffle=True)
+# Load video paths and labels
+actions = ['walking', 'jogging', 'running', 'boxing', 'handwaving', 'handclapping']
+video_files = []
+labels = []
 
-# Iterate through the dataloader
-for inputs, targets in dataloader:
-   print(inputs.shape, targets.shape)
+for i, action in enumerate(actions):
+    folder = os.path.join("data/KTH_Dataset", action)
+    for filename in os.listdir(folder):
+        if filename.endswith(".avi"):
+            path = os.path.join(folder, filename)
+            video_files.append(path)
+            labels.append(i)
+
+# Split data into training, validation, and test sets
+videos_train, videos_test, labels_train, labels_test = train_test_split(video_files, labels, test_size=0.2, random_state=42, stratify=labels)
+videos_train, videos_val, labels_train, labels_val = train_test_split(videos_train, labels_train, test_size=0.25, random_state=42, stratify=labels_train)
+
+train_data = [(v, l) for v, l in zip(videos_train, labels_train)]
+val_data = [(v, l) for v, l in zip(videos_val, labels_val)]
+test_data = [(v, l) for v, l in zip(videos_test, labels_test)]
+
+train_dataset = KTHDataset(train_data)
+val_dataset = KTHDataset(val_data)
+test_dataset = KTHDataset(test_data)
+
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=4)
+test_loader = DataLoader(test_dataset, batch_size=4)
+
+# Example of how to iterate through DataLoader
+for batch_idx, (data, target) in enumerate(train_loader):
+    print(data.shape, target)
